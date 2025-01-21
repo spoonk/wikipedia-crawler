0bef183eb319e7820e3bb8a57d4cc280ee6876a6
spoonk <cornwlucas@gmail.com>
2025-01-20 19:38:33 -0800

    cleanup wikipedia api

--- src/packages/apis/wikipedia-api.ts
+++ src/packages/apis/wikipedia-api.ts
@@ -5,6 +5,13 @@ import { channels } from "../utils/channels.js";
 
 const BASE_URL = "https://en.wikipedia.org/w/api.php";
 
+const defaultGetLinksParams = {
+  action: "query",
+  format: "json",
+  prop: "links",
+  pllimit: "max",
+};
+
 interface getResponse {
   query: {
     pages: {
@@ -22,53 +29,49 @@ interface getResponse {
   };
 }
 
-const getPageTitles = async (title: string, nextPageToken?: string | null) => {
-  const { data } = await axios.get<getResponse>(BASE_URL, {
-    params: {
-      action: "query",
-      format: "json",
-      prop: "links",
-      titles: title,
-      pllimit: "max",
-      ...(nextPageToken ? { plcontinue: nextPageToken } : {}),
-    },
-  });
+const getLinks = timedFunction(
+  async (title: string, nextPageToken?: string | null) => {
+    const { data } = await axios.get<getResponse>(BASE_URL, {
+      params: {
+        ...defaultGetLinksParams,
+        titles: title,
+        ...(nextPageToken ? { plcontinue: nextPageToken } : {}),
+      },
+    });
 
-  if (data.continue?.plcontinue) {
-    nextPageToken = data.continue.plcontinue;
-  } else {
-    nextPageToken = null;
-  }
+    if (data.continue?.plcontinue) {
+      nextPageToken = data.continue.plcontinue;
+    } else {
+      nextPageToken = null;
+    }
 
-  if (!data?.query?.pages) {
+    if (!data?.query?.pages) {
+      return {
+        links: [],
+        nextPageToken,
+      };
+    }
+
     return {
-      links: [],
+      links: _.map(
+        Object.values(data.query.pages)[0].links,
+        ({ title }) => title,
+      ),
       nextPageToken,
     };
-  }
-
-  return {
-    links: _.map(
-      Object.values(data.query.pages)[0].links,
-      ({ title }) => title,
-    ),
-    nextPageToken,
-  };
-};
-
-const timedGetPageTitles = timedFunction(getPageTitles, channels.apiTiming);
+  },
+  channels.apiTiming,
+);
 
 export async function getOutgoingPageTitles(title: string): Promise<string[]> {
   let token = null;
   const results = [];
 
   do {
-    const { links, nextPageToken } = await timedGetPageTitles(title, token);
+    const { links, nextPageToken } = await getLinks(title, token);
 
     token = nextPageToken;
     results.push(...links);
   } while (token);
   return results;
 }
-
-getOutgoingPageTitles("Albert Einstein");
